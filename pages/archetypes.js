16f04ac8a49f97e58bea8bda57cdb561ed86845e
MyMediaCreative <67966253+MyMediaCreative@users.noreply.github.com>
2022-10-23 12:07:42 -0400

    optimizing images

--- pages/archetypes.js
+++ pages/archetypes.js
@@ -10,21 +10,21 @@ import ArchCard from "../components/arch-card"
 const Archetypes = ({ homepage }) => {
 
     let artist       = { name: "The Artist", slug: "artist",
-                         image: "https://res.cloudinary.com/mymediacreative/image/upload/v1657362432/the%20harbour/archetype%20illustrations/Artist-square_ejnhps.png", }
+                         image: "https://res.cloudinary.com/mymediacreative/image/upload/c_scale,w_900/v1657362432/the%20harbour/archetype%20illustrations/Artist-square_ejnhps.png", }
     let caregiver    = { name: "The Caregiver", slug: "caregiver",
-                         image: "https://res.cloudinary.com/mymediacreative/image/upload/v1657362449/the%20harbour/archetype%20illustrations/Caregiver-square_qq0lqy.png", }
+                         image: "https://res.cloudinary.com/mymediacreative/image/upload/c_scale,w_900/v1657362449/the%20harbour/archetype%20illustrations/Caregiver-square_qq0lqy.png", }
     let storyteller  = { name: "The Storyteller", slug: "storyteller",
-                         image: "https://res.cloudinary.com/mymediacreative/image/upload/v1657362484/the%20harbour/archetype%20illustrations/Storyteller-square_wwleoc.png", }
+                         image: "https://res.cloudinary.com/mymediacreative/image/upload/c_scale,w_900/v1657362484/the%20harbour/archetype%20illustrations/Storyteller-square_wwleoc.png", }
     let organizer    = { name: "The Organizer", slug: "organizer",
-                         image: "https://res.cloudinary.com/mymediacreative/image/upload/v1657362470/the%20harbour/archetype%20illustrations/Organizer-square_h0ix8t.png", }
+                         image: "https://res.cloudinary.com/mymediacreative/image/upload/c_scale,w_900/v1657362470/the%20harbour/archetype%20illustrations/Organizer-square_h0ix8t.png", }
     let architect    = { name: "The Architect", slug: "architect",
-                         image: "https://res.cloudinary.com/mymediacreative/image/upload/v1657362444/the%20harbour/archetype%20illustrations/Architect-square_mtypwo.png", }
+                         image: "https://res.cloudinary.com/mymediacreative/image/upload/c_scale,w_900/v1657362444/the%20harbour/archetype%20illustrations/Architect-square_mtypwo.png", }
     let scientist    = { name: "The Scientist", slug: "scientist",
-                         image: "https://res.cloudinary.com/mymediacreative/image/upload/v1657362497/the%20harbour/archetype%20illustrations/Scientist-square_xec7ux.png", }
+                         image: "https://res.cloudinary.com/mymediacreative/image/upload/c_scale,w_900/v1657362497/the%20harbour/archetype%20illustrations/Scientist-square_xec7ux.png", }
     let entrepreneur = { name: "The Entrepreneur", slug: "entrepreneur",
-                         image: "https://res.cloudinary.com/mymediacreative/image/upload/v1657362414/the%20harbour/archetype%20illustrations/Entrepreneur-square_wqk53w.png", }
+                         image: "https://res.cloudinary.com/mymediacreative/image/upload/c_scale,w_900/v1657362414/the%20harbour/archetype%20illustrations/Entrepreneur-square_wqk53w.png", }
     let reformer     = { name: "The Reformer", slug: "reformer",
-                         image: "https://res.cloudinary.com/mymediacreative/image/upload/v1657362463/the%20harbour/archetype%20illustrations/Reformer-square_rcafgh.png", }
+                         image: "https://res.cloudinary.com/mymediacreative/image/upload/c_scale,w_900/v1657362463/the%20harbour/archetype%20illustrations/Reformer-square_rcafgh.png", }
 
   return (
     <>
